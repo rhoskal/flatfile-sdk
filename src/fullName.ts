1b445bfbd505734ca45c9c27b88c7277010127a3
Hans Hoffman <hansy@flatfile.lan>
2022-05-03 09:12:38 -0600

    add full name

---
+++ src/fullName.ts
@@ -0,0 +1,29 @@
+module.exports = async ({ recordBatch, _session, _logger }) => {
+  const fields = {
+    firstName: "firstName",
+    lastName: "lastName",
+  };
+
+  const isNil = (val) => val === null || val === undefined;
+
+  const isNotNil = (val) => !isNil(val);
+
+  await Promise.all(
+    await recordBatch.records.map(async (record) => {
+      const { firstName, lastName } = record.value;
+
+      if (firstName && isNotNil(lastName)) {
+        if (firstName.includes(" ")) {
+          const substrings = firstName.split(" ");
+
+          record
+            .set(fields.firstName, substrings[0])
+            .addComment(fields.firstName, "Full name was split");
+          record
+            .set(fields.lastName, substrings.join(" "))
+            .addComment(fields.lastName, "Full name was split");
+        }
+      }
+    }),
+  );
+};
