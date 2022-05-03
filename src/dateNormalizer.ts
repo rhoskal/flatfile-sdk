c689dac39ad985cbb8676e98fab1aeed29520304
Hans Hoffman <hansy@flatfile.lan>
2022-05-03 12:21:38 -0600

    remove promise from date normalizer

--- src/dateNormalizer.ts
+++ src/dateNormalizer.ts
@@ -9,29 +9,27 @@ const FIELDS = {
 };
 
 module.exports = async ({ recordBatch, _session, logger }) => {
-  return Promise.all(
-    await recordBatch.records.map((record) => {
-      Object.keys(FIELDS).forEach((field) => {
-        const { date: dateValue } = record.get(field);
+  return recordBatch.records.map((record) => {
+    Object.keys(FIELDS).forEach((field) => {
+      const { date: dateValue } = record.get(field);
 
-        if (isNotNil(dateValue)) {
-          if (Date.parse(dateValue)) {
-            const thisDate = dfns.format(new Date(dateValue), "yyyy-MM-dd");
-            const realDate = dfns.parseISO(thisDate);
+      if (isNotNil(dateValue)) {
+        if (Date.parse(dateValue)) {
+          const thisDate = dfns.format(new Date(dateValue), "yyyy-MM-dd");
+          const realDate = dfns.parseISO(thisDate);
 
-            if (dfns.isDate(realDate)) {
-              record.set(FIELDS.date, thisDate).addComment(FIELDS.date, "Automatically formatted");
+          if (dfns.isDate(realDate)) {
+            record.set(FIELDS.date, thisDate).addComment(FIELDS.date, "Automatically formatted");
 
-              if (dfns.isFuture(realDate)) {
-                record.addError(FIELDS.date, "Date cannot be in the future");
-              }
+            if (dfns.isFuture(realDate)) {
+              record.addError(FIELDS.date, "Date cannot be in the future");
             }
-          } else {
-            record.addError(FIELDS.date, "Invalid date");
-            logger.error("Invalid date");
           }
+        } else {
+          record.addError(FIELDS.date, "Invalid date");
+          logger.error("Invalid date");
         }
-      });
-    }),
-  );
+      }
+    });
+  });
 };
