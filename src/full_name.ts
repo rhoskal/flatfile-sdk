510b61b2cb5cf80057a05e6b1de0e2beed7baebd
Hans Hoffman <hansy@flatfile.lan>
2022-05-04 14:49:53 -0600

    fix empty string problem and overwriting lastName

--- src/full_name.ts
+++ src/full_name.ts
@@ -4,7 +4,7 @@
  *
  * @param {*} val - Any object/value
  */
-const isNil = (val) => val === null || val === undefined;
+const isNil = (val) => val === null || val === undefined || val === "";
 
 /**
  * Helper function to determine if a value is NOT null or undefined.
@@ -23,14 +23,13 @@ module.exports = async ({ recordBatch, _session, _logger }) => {
   return recordBatch.records.map((record) => {
     const { firstName, lastName } = record.value;
 
-    if (isNotNil(firstName) && isNotNil(lastName)) {
+    if (isNotNil(firstName) && isNil(lastName)) {
       if (firstName.includes(" ")) {
         const parts = firstName.split(" ");
 
         record
           .set(FIELDS.firstName, parts[0])
-          .addComment(FIELDS.firstName, "Full name was split");
-        record
+          .addComment(FIELDS.firstName, "Full name was split")
           .set(FIELDS.lastName, parts.slice(1, parts.length).join(" ").trim())
           .addComment(FIELDS.lastName, "Full name was split");
       }
